cbe2a96779707a98f506ed55fcded936437703bf
Sylvain Duchesne <pro@sylvainduchesne.com>
2013-02-19 18:22:37 +0100

    update pour Ced

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-angular.module('directive', []).directive('contenteditable', function() {
+﻿angular.module('letsco', []).config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/what', {templateUrl: 'partials/what.html', controller: WhatCtrl}).
+		when('/how', {templateUrl: 'partials/what.html', controller: HowCtrl}).
+		when('/why', {templateUrl: 'partials/what.html', controller: WhyCtrl}).
+		when('/who', {templateUrl: 'partials/who.html', controller: WhoCtrl}).
+		when('/where', {templateUrl: 'partials/where.html', controller: WhereCtrl}).
+		otherwise({redirectTo: '/'});
+}]);
+
+
+/*function Cntl($scope) {
+    $scope.updateBudgets = function(b, b_total) {
+        $scope.budget_finance;
+    }
+	
+	.directive('contenteditable', function() {
     return {
         require: 'ngModel',
         link: function(scope, elm, attrs, ctrl) {
@@ -16,11 +32,5 @@ angular.module('directive', []).directive('contenteditable', function() {
             ctrl.$setViewValue(elm.html());
         }
     };
-});
-
-
-/*function Cntl($scope) {
-    $scope.updateBudgets = function(b, b_total) {
-        $scope.budget_finance;
-    }
+})
 */
