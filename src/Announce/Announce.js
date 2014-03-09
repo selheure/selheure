b6a865015d66f03ce44fc8c350a6154b7b5492dc
Da Silva Jonathan <dasilvabastos.jonathan@gmail.com>
2014-03-09 15:40:57 +0100

    Correct some error in the importation, and also redo the identation of announce.

--- src/Announce/Announce.js
+++ src/Announce/Announce.js
@@ -1,25 +1,26 @@
 ng.factory('announces', function($q, db) {
   return {
     getLastAnnounces: function(nb) {
-      var _this = this;
       var options = {descending: true};
-      if(nb)
+      if(typeof nb == 'number') {
         options.limit = nb;
-      return db.getView('selheure', 'announcesByDate', [], false, options)
-      .then(function(list) {
-        var result = [];
-        var defCounter = 0;
-        var deferred = $q.defer();
-        function isFinished(announce) {
-          defCounter -= 1;
-          if(defCounter <= 0)
-            deferred.resolve(result);
-          return announce;
-        }
-        angular.forEach(list, function(_id) {
-          defCounter += 1;
-          result.push(_this.getAnnounce(_id).then(isFinished));
-          //destObject.push(_this.getAnnounce(_id));
+      }
+
+      return db.getView('selheure', 'announcesByDate', [], false, options).then(
+        function(list) {
+          var result = [];
+          var defCounter = 0;
+          var deferred = $q.defer();
+          function isFinished(announce) {
+            defCounter -= 1;
+            if(defCounter <= 0)
+              deferred.resolve(result);
+            return announce;
+          }
+          angular.forEach(list, function(_id) {
+            defCounter += 1;
+            result.push(_this.getAnnounce(_id).then(isFinished));
+            //destObject.push(_this.getAnnounce(_id));
         });
         return deferred.promise;
       })
