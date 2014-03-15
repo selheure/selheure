aa5a7d2410b6d7121d82f063b9a2f773a96e7cba
Da Silva Jonathan <dasilvabastos.jonathan@gmail.com>
2014-03-15 12:09:56 +0100

    Switch to the dbSelect Modules.

--- src/Selheure/__init__.js
+++ src/Selheure/__init__.js
@@ -1,9 +1,5 @@
-angular.module('selheure', ['ngRoute', 'url', 'user']).
-value('config', {
-  db: '',
-  lang: 'fr',
-  urlPrefix: '#',
-  collectifName: "CRIC",
-  translationDocPrefix: 'translation',
-  announceTypes: {demand: "demande", proposal: "proposition"},
+angular.module('selheure', ['ngRoute', 'url', 'user', 'dbSelect']).
+value('db', {
+  url:  '',
+  name: 'selheure',
 });
