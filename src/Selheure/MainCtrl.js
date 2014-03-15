b7659c606095d61196932c7301158594ecc72a51
Da Silva Jonathan <dasilvabastos.jonathan@gmail.com>
2014-03-15 12:38:46 +0100

    Add some submodule and make some change.

--- src/Selheure/MainCtrl.js
+++ src/Selheure/MainCtrl.js
@@ -1,18 +1,2 @@
-function MainCtrl($scope, url, config){
-	$scope.loginSubmit = function() {
-		console.log("login attempt");
-		if($scope.username && $scope.password){
-		}
-	};
-
-	$scope.signup = function() {
-		$('#signUpModal').modal();
-	}
-
-	$scope.signUpSubmit = function() {
-		$('#signUpModal').modal('hide');
-		//var newUser = {name: $scope.user.id};
-		var userDoc = angular.copy($scope.user);
-		console.log("userDoc", userDoc)
-	}
+function MainCtrl($scope, login){
 }
